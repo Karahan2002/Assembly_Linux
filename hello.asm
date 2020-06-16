ec0ba117b9d59138681bb282b4af05cd43d08376
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2020-06-16 17:39:02 +0200

    Create hello.asm

---
+++ hello.asm
@@ -0,0 +1,17 @@
+section .data
+        text db "Hello World",10
+
+section .text
+        global _start
+
+_start:
+        ; sys_write(1,text,14)
+        mov rax,1
+        mov rdi,1
+        mov rsi,text
+        mov rdx,14
+        syscall
+        ; sys_exit(0)
+        mov rax,60
+        mov rdi,0
+        syscall
